f24be770c5521acdd8d294401e6793ce03cee873
Sumit Pathak <sumit.pathak0606@gmail.com>
2024-08-09 23:52:51 +0530

    final commit: modified splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,4 @@
-
 import 'package:flutter/material.dart';
-
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -25,7 +23,28 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.asset('assets/logo.png'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome TO',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24.0,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Image.asset('assets/logo.png'),
+            const SizedBox(height: 10.0),
+            const Text(
+              'E-Commerce App',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24.0,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
