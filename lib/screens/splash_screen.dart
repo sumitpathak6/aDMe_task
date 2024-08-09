09d7642d325af92125bd3035680be0478c1c8592
Sumit Pathak <sumit.pathak0606@gmail.com>
2024-08-09 19:12:04 +0530

    feat: added splash screen navigation and primary routes

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 2), () {});
+    Navigator.pushReplacementNamed(context, '/home');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/logo.png'),
+      ),
+    );
+  }
+}
